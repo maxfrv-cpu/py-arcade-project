c1c850fa6268d2c84b86700b92517ae81917148e
Maxim <maxfrv14@gmail.com>
2026-01-29 21:12:50 +0300

    add player animation for direction

--- src/player.py
+++ src/player.py
@@ -4,25 +4,36 @@ import arcade
 class Player(arcade.Sprite):
     def __init__(self):
         super().__init__("resources/player1.png")
-        self.speed = 200  
-
+        self.speed = 200
         self.center_x = 704-24
         self.center_y = 1280-1232+24
+        
+        # Направления: 0=вверх, 1=вправо, 2=вниз, 3=влево
+        self.direction = 0
+        self.textures = [
+            arcade.load_texture("resources/player1.png"),  # Вверх
+            arcade.load_texture("resources/player2.png"),  # Вправо
+            arcade.load_texture("resources/player3.png"),  # Вниз
+            arcade.load_texture("resources/player4.png"),  # Влево
+        ]
 
-
-    def update(self, delta_time = 1 / 60, keys_pressed=None):
+    def update(self, delta_time=1/60, keys_pressed=None):
         if keys_pressed is None:
             keys_pressed = set()
 
         dx, dy = 0, 0
         if arcade.key.LEFT in keys_pressed or arcade.key.A in keys_pressed:
             dx -= self.speed * delta_time
+            self.direction = 3  # Влево
         if arcade.key.RIGHT in keys_pressed or arcade.key.D in keys_pressed:
             dx += self.speed * delta_time
+            self.direction = 1  # Вправо
         if arcade.key.UP in keys_pressed or arcade.key.W in keys_pressed:
             dy += self.speed * delta_time
+            self.direction = 0  # Вверх
         if arcade.key.DOWN in keys_pressed or arcade.key.S in keys_pressed:
             dy -= self.speed * delta_time
+            self.direction = 2  # Вниз
 
         if dx != 0 and dy != 0:
             factor = 0.7071
@@ -31,3 +42,6 @@ class Player(arcade.Sprite):
 
         self.center_x += dx
         self.center_y += dy
+        
+        # Обновляем текстуру
+        self.texture = self.textures[self.direction]
